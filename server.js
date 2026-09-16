41a66733503cbb496f75fe194223ec2ea8ed58c5
ChyyamakaU <amakaumunna90@gmail.com>
2026-09-16 10:41:42 +0100

    ft: add serve code

---
+++ server.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+const app = require("./app")
+require("dotenv").config();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen (PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
