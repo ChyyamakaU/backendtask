8350d74e54b1e7ff030e88d6cddc20714b1d6347
ChyyamakaU <amakaumunna90@gmail.com>
2026-09-17 20:15:39 +0100

    fix: register validator

--- src/validators/register.js
+++ src/validators/register.js
@@ -12,20 +12,7 @@ const validateRegister = (req, res, next) => {
   next();
 };
 
-const validateLogin = (req, res, next) => {
-  const { email, password } = req.body;
-
-  if (!email || !password) {
-    return res.status(400).json({
-      status: "error",
-      message: "Email and password are required"
-    });
-  }
-
-  next();
-};
 
 module.exports = {
   validateRegister,
-
 };
