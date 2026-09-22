e79997884a157cf63002a8fe5ba58054522e30e1
ChyyamakaU <amakaumunna90@gmail.com>
2026-09-22 01:23:19 +0100

    ft: add database of products

---
+++ src/database.js
@@ -0,0 +1,18 @@
+//Users
+
+const users = [];
+
+const products = [
+    {id:1, name: "Fruits", description: "fresh fruits", price: 105},
+    {id:2, name: "Electronics", description: "ALl quality elctronics", price: 2080},
+    {id:3, name: "Furnitures", description: "Hand made furnitures", price: 2322},
+    {id:4, name: "Cosmetics", description: "quality cosmetics", price: 155},
+    {id:5, name: "stationaries", description: "all office and school stationaries", price: 2345}
+]
+
+
+// eslint-disable-next-line no-undef
+module.exports = {
+    users,
+    products
+};
